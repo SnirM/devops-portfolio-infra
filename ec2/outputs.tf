8ddfa36aff54f0a7d7273fc84d4c202f65c43a80
Snir_DevOps <78410805+SnirM@users.noreply.github.com>
2026-01-08 18:41:19 +0200

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,4 +1,19 @@
 output "ec2_public_ip" {
-  value = aws_instance.app.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app.public_ip
 }
 
+output "app_url" {
+  description = "URL of the application (HTTP)"
+  value       = "http://${aws_instance.app.public_ip}"
+}
+
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.app.id
+}
+
+output "security_group_id" {
+  description = "Security Group ID"
+  value       = aws_security_group.app_sg.id
+}
