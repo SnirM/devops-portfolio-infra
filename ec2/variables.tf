5be51693591680d9b28a6d0051534a885a64277d
Snir_DevOps <78410805+SnirM@users.noreply.github.com>
2026-01-08 18:41:05 +0200

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,10 +1,33 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
 variable "ami_id" {
-  description = "Amazon Linux 2 AMI"
+  description = "Amazon Linux 2 AMI ID for the region"
   type        = string
 }
 
 variable "ecr_repo" {
-  description = "ECR repository URL"
+  description = "Full ECR repository URI INCLUDING repo name (no tag). Example: 8076....amazonaws.com/devops-portfolio-app"
   type        = string
 }
 
+variable "ecr_registry" {
+  description = "ECR registry hostname only. Example: 8076....dkr.ecr.ap-south-1.amazonaws.com"
+  type        = string
+  default     = "807650718119.dkr.ecr.ap-south-1.amazonaws.com"
+}
+
+variable "key_name" {
+  description = "Optional EC2 Key Pair name (leave empty if you will use SSM instead of SSH)"
+  type        = string
+  default     = ""
+}
