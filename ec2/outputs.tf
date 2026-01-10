7fa3a7938ea97ac8120dff2f87c093b6d0f1eab2
Snir_DevOps <78410805+SnirM@users.noreply.github.com>
2026-01-10 15:30:23 +0200

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,14 +1,3 @@
 output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.app.public_ip
-}
-
-output "ec2_public_dns" {
-  description = "Public DNS of the EC2 instance"
-  value       = aws_instance.app.public_dns
-}
-
-output "security_group_id" {
-  description = "Security group ID"
-  value       = aws_security_group.app_sg.id
+  value = aws_instance.app.public_ip
 }
